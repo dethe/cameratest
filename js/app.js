216d5bff5f5d76daf3f043d9d8a77d2cf61ff20f
dethe <dethe@livingcode.org>
2014-08-20 11:14:46 -0700

    added moz-specific camera api

--- js/app.js
+++ js/app.js
@@ -5,18 +5,34 @@ var height = window.screen.availHeight;
 video.setAttribute('width', Math.min(width, height));
 video.setAttribute('height', Math.min(width, height));
 
-navigator.getMedia = ( navigator.getUserMedia ||
-   navigator.webkitGetUserMedia ||
-   navigator.mozGetUserMedia ||
-   navigator.msGetUserMedia);
-var videoStream = navigator.getMedia({video:true}, function(stream) {
-    if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-    } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
-    }
+var cameraManager = navigator.mozCameras ? navigator.mozCameras[0] : navigator.mozCamera;
+
+function getPreview(camera){
+    var sizes = camera.capabilities.videoSizes;
+    camera.getPreviewVideoMode({size: sizes[sizes.length - 1]}, showPreview, errHandler);
+}
+
+function showPreview(stream){
+    var vendorURL = window.URL || window.webkitURL;
+    video.src = vendorURL.createObjectURL(stream);
     video.play();
-},function(err){
-    console.log('an error occurred: %o', err);
-});
+}
+
+function errHandler(errString){
+    console.log('Error: %s', errString);
+}
+
+if (cameraManager) {
+    var cameras = cameraManager.getListOfCameras();
+    for (var i = 0; i < cameras.length; i++){
+        console.log('camera #%s: %o', i, cameras[i]);
+    }
+    // get camera
+    cameraManager.getCamera({camera: camera[cameras.length - 1]}, getPreview);
+} else {
+    navigator.getMedia = ( navigator.getUserMedia ||
+       navigator.webkitGetUserMedia ||
+       navigator.mozGetUserMedia ||
+       navigator.msGetUserMedia);
+    var videoStream = navigator.getMedia({video:true}, showPreview, errHandler);
+}
